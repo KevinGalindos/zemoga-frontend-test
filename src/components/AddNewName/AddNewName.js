d49c3059a42b6ef88000188f9ffecce1aab3a79a
KevinGalindo <kevingalindos@gmail.com>
2021-04-14 23:42:21 -0500

    feat( complete) web interface still on work responsive not yet

--- src/components/AddNewName/AddNewName.js
+++ src/components/AddNewName/AddNewName.js
@@ -1,7 +1,16 @@
 export const AddNewName = () => {
   return (
     <div className="AddNewName">
-      <div className="container"></div>
+      <div className="container">
+        <div className="wrapper">
+          <div className="text">
+            <h2>Is there anyone else you would want us to add?</h2>
+          </div>
+          <div className="btn-submit">
+            <button>Submit a name</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
